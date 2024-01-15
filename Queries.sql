5dcab08cd48284dacde6c5f105925c6772b70af8
Ferhat Demirci <ferhat.demirci@gmail.com>
2024-01-15 21:29:45 +0300

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -272,6 +272,23 @@ Select * from TOP_5_GENRES;
 --8c. You find that you no longer need the view top_five_genres. Write a query to delete it.
 DROP VIEW TOP_5_GENRES;
 
+-- Kiralanan filmleri henüz getirmemiş müşterileri listele. Adı, soyadı, telefonu, aldığı film ve kiralama tarihi
 
+SELECT
+	CONCAT(customer.last_name, ', ', customer.first_name) AS customer,
+	address.phone,
+	film.title
+FROM
+	rental
+	INNER JOIN customer ON rental.customer_id = customer.customer_id
+	INNER JOIN address ON customer.address_id = address.address_id
+	INNER JOIN inventory ON rental.inventory_id = inventory.inventory_id
+	INNER JOIN film ON inventory.film_id = film.film_id
+WHERE
+	rental.return_date IS NULL
+	AND rental_date < CURRENT_DATE
+ORDER BY
+	title
+LIMIT 5;
 
 
